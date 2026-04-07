8ca862c4841358a308d4949c60624a3a96033287
jeremy <pressline213100@gmail.com>
2026-04-07 11:28:06 +0800

    111

--- lib/providers/cloud_sync_provider.dart
+++ lib/providers/cloud_sync_provider.dart
@@ -1,4 +1,6 @@
-import 'dart:io';
+import 'dart:io' as io;
+import 'dart:convert';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:file_picker/file_picker.dart';
 import 'package:shared_preferences/shared_preferences.dart';
@@ -33,6 +35,12 @@ class CloudSyncProvider extends ChangeNotifier {
     _isSyncing = true;
     notifyListeners();
     try {
+      if (kIsWeb) {
+         print('Export is not fully supported on web yet without dart:html');
+         _isSyncing = false;
+         notifyListeners();
+         return false; // Skip for now so it compiles
+      }
       String? outputFile = await FilePicker.platform.saveFile(
         dialogTitle: 'Save Backup',
         fileName: 'schonotes_backup.json',
@@ -40,7 +48,7 @@ class CloudSyncProvider extends ChangeNotifier {
         allowedExtensions: ['json'],
       );
       if (outputFile != null) {
-        final File file = File(outputFile);
+        final file = io.File(outputFile);
         await file.writeAsString(jsonData);
         _lastSyncTime = DateTime.now();
         _saveState();
@@ -64,9 +72,23 @@ class CloudSyncProvider extends ChangeNotifier {
         type: FileType.custom,
         allowedExtensions: ['json'],
       );
-      if (result != null && result.files.single.path != null) {
-        final File file = File(result.files.single.path!);
-        final data = await file.readAsString();
+      if (result != null) {
+        String data = '';
+        if (kIsWeb) {
+           final bytes = result.files.single.bytes;
+           if (bytes != null) {
+              data = utf8.decode(bytes);
+           } else {
+             throw Exception("No bytes found in web file picker");
+           }
+        } else {
+           if (result.files.single.path != null) {
+              final file = io.File(result.files.single.path!);
+              data = await file.readAsString();
+           } else {
+             throw Exception("No path found in file picker");
+           }
+        }
         _lastSyncTime = DateTime.now();
         _saveState();
         _isSyncing = false;
